afb4500a0d4fb705759d2e7b50427ed48b0486f2
yenipujiati <yeni.p19@student.ukrimuniversity.ac.id>
2021-09-19 02:14:53 +0700

    setting halaman login menjadi awal

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\AuthController::class, 'index']);
 
 Route::get('/login', [App\Http\Controllers\AuthController::class, 'index'])->name('auth.index');
 
