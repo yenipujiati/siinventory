79f4efe45976e0cb1fa7b294a24ad4343cbd076d
telaumbanuaberkati <berkati.t19@student.ukrimuniversity>
2021-09-20 22:07:11 +0700

    create case converter

--- app/Util/Helper.php
+++ app/Util/Helper.php
@@ -13,4 +13,12 @@ class Helper
     public static function rupiahConverter($number) {
         return "Rp".number_format($number,2,',',',');
     }
+
+    public static function lowercaseConverter($text) {
+        return strtolower($text);
+    }
+
+    public static function uppercaseConverter($text) {
+        return strtoupper($text);
+    }
 }
